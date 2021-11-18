7e28a170d1c982f05dff20dccf6f928e51a8d183
EmmanuelNiyi <emmanuelniyioriolowo@gmail.com>
2021-11-18 22:44:58 +0100

    made the previous commit a function which returns 'empty' or 'not empty' based on the content of the folder

--- notion_anki_auto.py
+++ notion_anki_auto.py
@@ -3,17 +3,24 @@ import os
 
 
 
-print(f"The current working directory is {os.getcwd()}")
-os.chdir("C:\\Users\\emman\\Projects\\notion_anki_auto")
-print(f"The current working directory is {os.getcwd()}")
+# print(f"The current working directory is {os.getcwd()}")
+# os.chdir("C:\\Users\\emman\\Projects\\notion_anki_auto")
+# print(f"The current working directory is {os.getcwd()}")
 
 # path ="C:\\Users\\emman\\Projects\\notion_anki_auto" 
-path ="C:\\Users\\emman\\Documents\\To Anki"
-directory= os.listdir(path) 
-if len(directory) == 0: 
+
+def check_for_new_decks():
+    path ="C:\\Users\\emman\\Documents\\To Anki"
+    directory= os.listdir(path) 
+    if len(directory) == 1: 
+        print(f"The number of items in the directory is {len(directory)}")
+        print(directory)
+        print("The folder is empty \nThere are no new decks") 
+        return "Empty"
+    else: 
+        print(f"The number of items in the directory is {len(directory)}")
+        print(directory)
+        print("There is more than one item in the folder \nThere are new decks")
+        return "Not Empty"
     
-    print("Empty directory") 
-else: 
-    print("Not empty directory")
-    print(directory)
-    print(f"The number of items in the directory is {len(directory)}")
+print(check_for_new_decks())
