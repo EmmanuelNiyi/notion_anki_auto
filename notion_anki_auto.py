2e72899b4de621cf5a9ee060d0db88e97977abc0
root <root@Emmanuel.localdomain>
2021-12-21 00:30:37 +0100

    Fully Functional now

--- notion_anki_auto.py
+++ notion_anki_auto.py
@@ -1,10 +1,10 @@
 import sys
 import os
 import time
+import shutil
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from apscheduler.schedulers.background import BackgroundScheduler
 
 
 
@@ -12,12 +12,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 path ="C:\\Users\\emman\\Documents\\To Anki"
 directory= os.listdir(path) 
 
-
-# print(f"The current working directory is {os.getcwd()}")
-# os.chdir("C:\\Users\\emman\\Projects\\notion_anki_auto")
-# print(f"The current working directory is {os.getcwd()}")
-
-# path ="C:\\Users\\emman\\Projects\\notion_anki_auto" 
 
 def check_for_new_decks():
     if len(directory) == 1: 
@@ -30,6 +24,23 @@ def check_for_new_decks():
         print(directory)
         print("There is more than one item in the folder \nThere are new decks")
         return "Not Empty"
+
+
+def move_converted_decks_to_exportedfolder():
+    print("In Move converted item to exported folder")
+
+    for item in directory:
+        if item == "Exported":  
+            print("This is the 'Exported' folder, NO TRESPASSING ")
+            pass
+        else:
+            print(f"We are moving {item}")
+            source = f"C:\\Users\\emman\\Documents\\To Anki\\{item}"
+            destination = f"C:\\Users\\emman\\Documents\\To Anki\\Exported\\Pediatrics\\{item}"
+            shutil.move(source,destination)
+
+            print(f"{item} has been moved from {source} to {destination} .")
+
     
 def convert_notion_to_anki():
     # using web scraping 
@@ -52,9 +63,14 @@ def convert_notion_to_anki():
             print("The path is", f"C:\\Users\\emman\\Documents\\To Anki\\{item}")
             print(f"C:\\Users\\emman\\Documents\\To Anki\\{item}")
             file_input.send_keys(f"C:\\Users\\emman\\Documents\\To Anki\\{item}")
+
         
     time.sleep(10)
     driver.find_element(By.CSS_SELECTOR, "#root > div > div > div > div.container > form > div > div > a").click()
+
+    print("Downloaded")
+    time.sleep(5)
+    driver.close()
 
 # print(check_for_new_decks())
 
@@ -62,34 +78,12 @@ if check_for_new_decks() == "Empty":
     print("No new decks")
 else:
 
-    driver = webdriver.Edge(executable_path='C:/Users/emman/Desktop/stuff/edgedriver_win64/msedgedriver.exe')
+    driver = webdriver.Chrome(executable_path='C:/Users/emman/Desktop/stuff/chromedriver_win32/chromedriver.exe')
     driver.get("https://2anki.net/upload")
     driver.maximize_window()
 
     convert_notion_to_anki()
     print("Converted")
 
-
-# def check_and_convert():
-#     if check_for_new_decks() == "Empty":
-#         print("No new decks")
-#     else:
-#         convert_notion_to_anki()
-#         print("Converted")
-
-# def scheduler_test():
-#     print("Scheduler Working")
-
-
-# def start():
-#     scheduler = BackgroundScheduler()
-#     """TESTING"""
-#     scheduler.add_job(scheduler_test, 'cron', hour='12,14,17',
-#                       minute='0,5,10,15,20,25,34,35,38', )
-
-#     """chron not assigned"""
-
-#     """WORKING"""
-#     # Runs in 1:00am everyday
-#     scheduler.add_job(check_and_convert, 'interval', minutes=30)
-#     print("Scheduler started...", file=sys.stdout)
+    move_converted_decks_to_exportedfolder()
+    print("Moved")
